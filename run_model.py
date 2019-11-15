a4533d6e663ced73a73608d0d93449b2db2f7af7
Teddy Groves <groves.teddy@gmail.com>
2019-11-15 17:52:45 +0100

    Improve main script and add plotting

--- run_model.py
+++ run_model.py
@@ -34,15 +34,16 @@ def run_enzyme_model(data_df):
     input_prior = {**input_base, **{'likelihood': 0}}
     input_posterior = {**input_base, **{'likelihood': 1}}
 
-    fit_prior = model.sample(data=input_prior, adapt_delta=0.999, csv_basename='./enzyme_output.csv')
+    fit_prior = model.sample(data=input_prior, adapt_delta=0.999, max_treedepth=12)
     fit_prior.diagnose()
-    fit_posterior = enzyme_model.sample(data=input_posterior, adapt_delta=0.999)
+    fit_posterior = model.sample(data=input_posterior, adapt_delta=0.999, max_treedepth=12)
     fit_posterior.diagnose()
     infd_prior, infd_posterior = (
         az.from_cmdstanpy(
             fit,
             coords={'enzyme_labels': data_df.groupby('ec4_stan')['ec_number'].first().values},
-            dims={'mu_enzyme': ['enzyme_labels']}
+            dims={'mu_enzyme': ['enzyme_labels']},
+            log_likelihood='log_likelihood'
         )
         for fit in [fit_prior, fit_posterior]
     )
@@ -62,9 +63,9 @@ def run_ec3_model(data_df):
     }
     input_prior = {**input_base, **{'likelihood': 0}}
     input_posterior = {**input_base, **{'likelihood': 1}}
-    fit_prior = model.sample(data=input_prior, adapt_delta=0.999, csv_basename='./ec3_output.csv')
+    fit_prior = model.sample(data=input_prior, adapt_delta=0.999, max_treedepth=12)
     fit_prior.diagnose()
-    fit_posterior = model.sample(data=input_posterior, adapt_delta=0.999)
+    fit_posterior = model.sample(data=input_posterior, adapt_delta=0.999, max_treedepth=12)
     fit_posterior.diagnose()
     infd_prior, infd_posterior = (
         az.from_cmdstanpy(
@@ -73,7 +74,8 @@ def run_ec3_model(data_df):
                 'enzyme_labels': data_df.groupby('ec4_stan')['ec_number'].first().values,
                 'ec3_labels': data_df.groupby('ec3_stan')['ec3'].first().values
             },
-            dims={'mu_enzyme': ['enzyme_labels'], 'mu_ec3': ['ec3_labels'], 'sigma_ec3': ['ec3_labels']}
+            dims={'mu_enzyme': ['enzyme_labels'], 'mu_ec3': ['ec3_labels'], 'sigma_ec3': ['ec3_labels']},
+            log_likelihood='log_likelihood'
         )
         for fit in [fit_prior, fit_posterior]
     )
@@ -90,28 +92,39 @@ def plot_forest(ax, df, low=0.1, high=0.9, color='tab:blue', alpha=0.5):
     ax.set_yticklabels(df.columns)
     return ax
 
+
+def plot_param_forest(infd_prior, infd_posterior, par, ax):
+    samples_prior, samples_posterior = (
+        p[par].to_series().unstack().pipe(sort_by_median)
+        for p in [infd_prior.posterior, infd_posterior.posterior]
+    )
+    plot_forest(ax, samples_prior, color='tab:orange')
+    plot_forest(ax, samples_posterior)
+    ax.set_xlabel(par)
+    return ax
+
+
 def sort_by_median(df, axis=1):
     return df.reindex(df.median().sort_values().index, axis=axis)
 
 
 def main():
+    # get data
     data_df = get_data_df()
+
+    # run models
     _, _, infd_prior_enzyme, infd_posterior_enzyme = run_enzyme_model(data_df)
     _, _, infd_prior_ec3, infd_posterior_ec3 = run_ec3_model(data_df)
+
+    # draw plots
+    f, ax = plt.subplots(figsize=[10, 25])
+    ax = plot_param_forest(infd_prior_ec3, infd_posterior_ec3, 'log_sigma_ec3', ax)
+    plt.savefig('sigma_forest.png')
+    plt.close('all')
 
 
 if __name__ == '__main__':
     main()
 
-# log_sigma_samples_prior = infd_prior.posterior['log_sigma_ec3'].to_series().unstack().pipe(sort_by_median)
-# log_sigma_samples_posterior = infd_posterior.posterior['log_sigma_ec3'].to_series().unstack().pipe(sort_by_median)
-# f, ax = plt.subplots(figsize=[10, 25])
-# plot_forest(ax, log_sigma_samples_prior, color='tab:orange')
-# plot_forest(ax, log_sigma_samples_posterior)
 
-# ax.set_ylabel('EC3')
-# ax.set_xlabel('Modelled group standard deviation (log scale)')
-
-# plt.savefig('sigma_forest.png')
-# plt.close('all')
 
